cd815dd34963a393407563ef7b4b77c410a7254e
Alfonso Sanchez <nerdzila@gmail.com>
2022-03-11 11:40:58 -0600

    feat(Adapter): Add live example

--- design-pattern-adapter/cartItems/WebClientItemAdapter.ts
+++ design-pattern-adapter/cartItems/WebClientItemAdapter.ts
@@ -2,7 +2,7 @@ import { WebClientItem } from './WebClientItem';
 import { ZecoreItem } from './ZecoreItem';
 
 class WebClientItemAdapter implements ZecoreItem {
-
+  // Implementación pendiente
 }
 
 export default WebClientItemAdapter;
--- design-pattern-adapter/cartItems/ZecoreItemAdapter.ts
+++ design-pattern-adapter/cartItems/ZecoreItemAdapter.ts
@@ -2,7 +2,27 @@ import { ZecoreItem } from './ZecoreItem';
 import { WebClientItem } from './WebClientItem';
 
 class ZecoreItemAdapter implements WebClientItem {
+  protected zItem : ZecoreItem;
 
+  constructor (zItem: ZecoreItem) {
+    this.zItem = zItem;
+  }
+
+  get quantity() {
+    return this.zItem.quantity;
+  }
+
+  get product()  {
+    return {
+      id: this.zItem.itemCode,
+      name: this.zItem.itemName,
+      baselineRate: this.zItem.baselineRate,
+      finalRate: this.zItem.finalRate,
+      image: {
+       small: this.zItem.image
+     }
+    };
+  }
 }
 
 export default ZecoreItemAdapter;
